c95f816af48415b576ea8e50bf993f390b7ea739
NicolasBlair <58854689+NicolasBlair@users.noreply.github.com>
2020-06-02 02:12:10 +0200

    Update CSS TEST

--- finalWebSite/sonore.php
+++ finalWebSite/sonore.php
@@ -28,9 +28,9 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title>Réflexe sonore</title>
-		<link rel="stylesheet" href="css/tests/styleTemp.css" />
-		<link rel="stylesheet" href="css/tests/styleReflexe.css" />
+		<link rel="stylesheet" href="css/tests/styleTest.css" />
 		<script type="text/javascript" src="js/rebours.js"></script>
+		<script src="js/help.js"></script>
 	</head>
 
 	<body>
@@ -39,42 +39,35 @@
 
 	<div class="titre"><h2><?= trad("Réflexe sonore","Sound reflex") ?></h2></div>
 
-		<div class="contenu">
-			
+	<section class="conteneur1">
+		<section class="conteneur2">
+				
 			<div class="image">
+				<img src="img/test9.png" alt="sonore" title="<?=trad("Mesure à un stimulus sonore","Measurement to a sound stimulus")?>"/>
+			</div>
+
+			<div class="rebours"><img src="img/compteRebours.jpg" alt="compteRebours" title="<?=trad("Compte à rebours","Countdown")?>"/></div>
+
+			<div id="texte"></div>
+
+			<button type="button" class="boutonRebours" id="compte_a_rebours" onclick="rebours(30,90)"><?= trad("Démarrer","Start") ?></button>
+
+			<button type="button" class="boutonHelp" name="idHelp" onclick ="helpSonore()"><?= trad("Aide","Help") ?></button>
 			
-				<div class="compte">
-					<div class="rebours"><img src="img/compteRebours.jpg" alt="compteRebours" title="<?= trad("Compte à rebours","Countdown") ?>"/> 
-		    		</div>
-		    		<div id="cadre">
-		    			<div id="compte_a_rebours"></div>
-		    		</div>
-		    	</div>
-		    	<div class="sonore"><img src="img/test9.png" alt="sonore" title="sonore"/> 
-		    	</div>
-		    </div>
-
-			<!-- <div class="bouton"><p><a href="visuel.php" style="text-decoration:none">Réflexe visuel</a></p>
-			</div> -->
-
 			<form method="POST" action="visuel.php">
-
-				<button type="submit" class="bouton" name="idTest" value="<?= $idTest ?>"><?= trad("Réflexe visuel","Visual reflex") ?></button>
+				<button type="submit" class="boutonSubmit" name="idTest" value="<?= $idTest ?>"><?= trad("Réflexe visuel","Visual reflex") ?></button>
 			</form>
 
-			
 			<div class="barre">
-			<progress id="barreProgression" max="100" value="80"><?= trad("4/5 épreuves effectuées","4/5 tests performed") ?></progress><br/><br/>
-			<label for="barreProgression"><?= trad("4/5 épreuves effectuées","4/5 tests performed") ?></label>
-		    </div>
-			
+				<progress id="barreProgression" max="100" value="80"><?= trad("4/5 épreuves effectuées","4/5 tests performed") ?></progress><br/><br/>
+			</div>
 
-		</div>
+			<label for="barreProgression"><?= trad("4/5 épreuves effectuées","4/5 tests performed") ?></label>
+
+		</section>
+	</section>
 
 	<?php include("includes/footer.php"); ?>
 
-
-
 	</body>
-	
 </html>	
