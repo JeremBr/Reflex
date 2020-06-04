ee0021b34b8678e95444b4da018ddc62dc4e9500
franny1 <francoisjosephbouault@gmail.com>
2020-06-04 12:45:50 +0200

    css

--- InfiniteMeasures/vues/monCompte.php
+++ InfiniteMeasures/vues/monCompte.php
@@ -66,6 +66,7 @@
                                 <li><a class="caseAdmin" href="mail"><?php echo trad("Envoyer un mail","Send an email")?></a></li>
                                 <li><a class="caseAdmin" href="inviter"><?php echo trad("Inviter un utilisateur","Invite a user")?></a></li>
                                 <li><a class="caseAdmin" href="modifierFAQ"><?php echo trad("Gérer la F.A.Q","Change F.A.Q")?></a></li>
+                                <li><a class="caseAdmin" href="modifierCGU"><?php echo trad("Gérer la C.G.U","Change T.O.U")?></a></li>
                                 <li><a class="caseAdmin" href="modifierProfil"><?php echo trad("Editer mon profil","Update profil")?></a></li>
                                 <li><a class="caseAdmin" href="deconnexion"><?php echo trad("Déconnexion","Log out")?></a></li> 
 
